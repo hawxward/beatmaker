4bb2c77852124d628dd8f35da4c8492ee489f094
Alex Hawker <malex.leftforchance@gmail.com>
2019-07-18 18:52:37 +0100

    Updates

---
+++ precache-manifest.7a2bd0b231cda9f5e0b4708a591bca41.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45711ce31fa40b1ea690608491041de",
+    "url": "/beatmaker/index.html"
+  },
+  {
+    "revision": "f34b43aa397142e2b297",
+    "url": "/beatmaker/static/css/main.795641e1.chunk.css"
+  },
+  {
+    "revision": "86881a6d25496d993bb8",
+    "url": "/beatmaker/static/js/2.d1219a6b.chunk.js"
+  },
+  {
+    "revision": "f34b43aa397142e2b297",
+    "url": "/beatmaker/static/js/main.ebd43131.chunk.js"
+  },
+  {
+    "revision": "a40a96449e16b89850fc",
+    "url": "/beatmaker/static/js/runtime~main.a4683966.js"
+  },
+  {
+    "revision": "8d08371fcd707773237a646cfb615cfe",
+    "url": "/beatmaker/static/media/808hconga.8d08371f.wav"
+  },
+  {
+    "revision": "f2910c32d9a3f2db1f64d771b393cbb0",
+    "url": "/beatmaker/static/media/Clap02.f2910c32.wav"
+  },
+  {
+    "revision": "fb090900ed7f0fe3bbf9b2c5ee299e3b",
+    "url": "/beatmaker/static/media/Crash.fb090900.wav"
+  },
+  {
+    "revision": "b91b64ff68d9377c9030942b9a874b3e",
+    "url": "/beatmaker/static/media/HiHat.b91b64ff.wav"
+  },
+  {
+    "revision": "d3369824566837a10bfda2561155870a",
+    "url": "/beatmaker/static/media/Kick.d3369824.wav"
+  },
+  {
+    "revision": "3111c8495e244156a9e49b9d3dee6df8",
+    "url": "/beatmaker/static/media/Shaker.3111c849.wav"
+  },
+  {
+    "revision": "a3af7ccf5211a2ec572d8f20c1a436d0",
+    "url": "/beatmaker/static/media/SnareSound.a3af7ccf.wav"
+  },
+  {
+    "revision": "6399c42c8fc9615025cd21c3997249fc",
+    "url": "/beatmaker/static/media/Tom.6399c42c.wav"
+  },
+  {
+    "revision": "d43507e163a4188aa659ec1b56a6d860",
+    "url": "/beatmaker/static/media/drum pad.d43507e1.svg"
+  },
+  {
+    "revision": "59d2a27c3abf1db8546527c73edfd09a",
+    "url": "/beatmaker/static/media/drumSequencer.59d2a27c.svg"
+  }
+]);
